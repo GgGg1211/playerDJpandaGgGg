15741ceb979171fc8ea62ef55c7dfe5d3503b3de
GgGg1211 <7658450@qq.com>
2020-10-28 20:58:19 +0800

    试试

--- 谷小宇/git.md
+++
@@ -1,5 +0,0 @@
-gitfloow工作流程
-master分支是主分支 用于上线 （权限高）
-
-一般的开发环境是 dev分支
-gitgone 忽略文件夹
--- 谷小宇/src/utils/until.js
+++
@@ -1,6 +0,0 @@
-const time = (url,istrue)=>{
-    let date = new Date().getTime();
-    return istrue ? `${url}&_t=${date}` : `${url}?_t=${date}`
-}
-
-export{time}
